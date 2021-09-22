eede2d690d494ace2efee6cad5082353c26e17da
Juaneto <Juan.gonzalez36@uotc.edu.co>
2021-09-22 18:02:16 -0500

    Cambieishon

--- src/co/edu/uptc/presentacion/Menu.java
+++ src/co/edu/uptc/presentacion/Menu.java
@@ -2,6 +2,6 @@ package co.edu.uptc.presentacion;
 
 public class Menu {
     public void inicio(){
-        System.out.println("HOLA MUNDO");
+        System.out.println("Hola Capos y Capas");
     }
 }
