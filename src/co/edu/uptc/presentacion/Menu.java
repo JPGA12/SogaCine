944db44afdf82bbb6ce3e5e9e5cdfb02e7987aa4
JUAN <juan.gutierrez12@uptc.edu.co>
2021-09-22 16:52:00 -0500

    Inicio 1

---
+++ src/co/edu/uptc/presentacion/Menu.java
@@ -0,0 +1,7 @@
+package co.edu.uptc.presentacion;
+
+public class Menu {
+    public void inicio(){
+        System.out.println("HOLA MUNDO");
+    }
+}
---
+++ src/co/edu/uptc/presentacion/RunSogaCine.java
@@ -0,0 +1,7 @@
+package co.edu.uptc.presentacion;
+
+public class RunSogaCine {
+    public static void main(String[] args) {
+        new Menu().inicio();
+    }
+}
