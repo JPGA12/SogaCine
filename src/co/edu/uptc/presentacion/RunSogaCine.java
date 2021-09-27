89e5a703fb0cc8f3d3d1f217bb0e7bcfd4900e7a
JUAN <juan.gutierrez12@uptc.edu.co>
2021-09-27 18:05:16 -0500

    Bienvenida

--- src/co/edu/uptc/presentacion/RunSogaCine.java
+++ src/co/edu/uptc/presentacion/RunSogaCine.java
@@ -3,5 +3,7 @@ package co.edu.uptc.presentacion;
 public class RunSogaCine {
     public static void main(String[] args) {
         new Menu().Inicio();
+        System.out.println("Bienvenido");
+        System.out.println("Ejecutando");
     }
 }
