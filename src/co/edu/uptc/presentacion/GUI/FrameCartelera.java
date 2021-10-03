7d4fc121663979d17bbc3225ea7d2844be97981e
Juaneto <Juan.gonzalez36@uotc.edu.co>
2021-10-03 17:49:50 -0500

    CarteleraNueva

--- src/co/edu/uptc/presentacion/GUI/FrameCartelera.java
+++ src/co/edu/uptc/presentacion/GUI/FrameCartelera.java
@@ -7,7 +7,6 @@ import java.awt.event.*;
 public class FrameCartelera extends JFrame implements ActionListener{
 
     private JPanel panel;
-    private JLabel mensaje;
 
     public FrameCartelera(){
         setSize(400, 650);
@@ -31,11 +30,29 @@ public class FrameCartelera extends JFrame implements ActionListener{
         iconLogo.setBounds(20, 40, 350, 100);
         panel.add(iconLogo);
 
-        JButton Peli1 = new JButton();
-        Peli1.setBounds(20,300,150,200);
-        ImageIcon icon = new ImageIcon("1.png");
-        Peli1.setIcon(new ImageIcon(icon.getImage().getScaledInstance(Peli1.getWidth(),Peli1.getHeight(),Image.SCALE_SMOOTH)));
-        panel.add(Peli1);
+        JButton Boton1 = new JButton();
+        Boton1.setBounds(40,170,150,200);
+        ImageIcon icon1 = new ImageIcon("imagenes/1.png");
+        Boton1.setIcon(new ImageIcon(icon1.getImage().getScaledInstance(Boton1.getWidth(), Boton1.getHeight(),Image.SCALE_SMOOTH)));
+        panel.add(Boton1);
+
+        JButton boton2 = new JButton();
+        boton2.setBounds(200,170,150,200);
+        ImageIcon icon2 = new ImageIcon("imagenes/2.jpg");
+        boton2.setIcon(new ImageIcon(icon2.getImage().getScaledInstance(boton2.getWidth(), boton2.getHeight(),Image.SCALE_SMOOTH)));
+        panel.add(boton2);
+
+        JButton boton3 = new JButton();
+        boton3.setBounds(40,380,150,200);
+        ImageIcon icon3 = new ImageIcon("imagenes/3.jpg");
+        boton3.setIcon(new ImageIcon(icon3.getImage().getScaledInstance(boton3.getWidth(), boton3.getHeight(),Image.SCALE_SMOOTH)));
+        panel.add(boton3);
+
+        JButton boton4 = new JButton();
+        boton4.setBounds(200,380,150,200);
+        ImageIcon icon4 = new ImageIcon("imagenes/4.jpg");
+        boton4.setIcon(new ImageIcon(icon4.getImage().getScaledInstance(boton4.getWidth(), boton4.getHeight(),Image.SCALE_SMOOTH)));
+        panel.add(boton4);
     }
 
     @Override
