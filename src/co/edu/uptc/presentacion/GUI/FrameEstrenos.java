bff2094081c56be33912fd2fb010feb18793d0db
Juaneto <Juan.gonzalez36@uotc.edu.co>
2021-10-03 19:15:41 -0500

    Porfavor

--- src/co/edu/uptc/presentacion/GUI/FrameEstrenos.java
+++ src/co/edu/uptc/presentacion/GUI/FrameEstrenos.java
@@ -2,13 +2,16 @@ package co.edu.uptc.presentacion.GUI;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FrameCartelera extends JFrame implements ActionListener{
+public class FrameEstrenos extends JFrame implements ActionListener{
 
     private JPanel panel;
+    private JButton Boton1, Boton2, Boton3, Boton4;
+    private JLabel iconLogo, titulo;
 
-    public FrameCartelera(){
+    public FrameEstrenos(){
         setSize(400, 650);
         setTitle("SogaCine");
         setLocationRelativeTo(null);
@@ -26,49 +29,55 @@ public class FrameCartelera extends JFrame implements ActionListener{
         this.getContentPane().add(panel);
     }
     public void colocarEtiqueta(){
-        JLabel iconLogo = new JLabel(new ImageIcon("imagenes/LogoSoga.png"));
+        iconLogo = new JLabel(new ImageIcon("imagenes/LogoSoga.png"));
         iconLogo.setBounds(20, 20, 350, 100);
         panel.add(iconLogo);
 
-        JLabel titulo = new JLabel();
+        titulo = new JLabel();
         titulo.setBounds(20, 90, 350, 100);
-        titulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        titulo.setText("CARTELERA");
-        titulo.setFont(new Font("Tahoma", Font.BOLD, 30));
+        titulo.setHorizontalAlignment(SwingConstants.CENTER);
+        titulo.setText("PROXIMOS ESTRENOS");
+        titulo.setFont(new Font("Tahoma", 1, 30));
         titulo.setForeground(Color.WHITE);
         panel.add(titulo);
 
-        JButton Boton1 = new JButton();
+        Boton1 = new JButton();
         Boton1.setBounds(40,170,150,200);
-        ImageIcon icon1 = new ImageIcon("imagenes/1.png");
+        ImageIcon icon1 = new ImageIcon("imagenes/Proximos1.png");
         Boton1.setIcon(new ImageIcon(icon1.getImage().getScaledInstance(Boton1.getWidth(), Boton1.getHeight(),Image.SCALE_SMOOTH)));
         Boton1.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2, true));
+        Boton1.addActionListener(this);
         panel.add(Boton1);
 
-        JButton Boton2 = new JButton();
+        Boton2 = new JButton();
         Boton2.setBounds(200,170,150,200);
-        ImageIcon icon2 = new ImageIcon("imagenes/2.jpg");
+        ImageIcon icon2 = new ImageIcon("imagenes/Proximos2.jpg");
         Boton2.setIcon(new ImageIcon(icon2.getImage().getScaledInstance(Boton2.getWidth(), Boton2.getHeight(),Image.SCALE_SMOOTH)));
         Boton2.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2, true));
+        Boton2.addActionListener(this);
         panel.add(Boton2);
 
-        JButton Boton3 = new JButton();
+        Boton3 = new JButton();
         Boton3.setBounds(40,380,150,200);
-        ImageIcon icon3 = new ImageIcon("imagenes/3.jpg");
+        ImageIcon icon3 = new ImageIcon("imagenes/Proximos3.jpg");
         Boton3.setIcon(new ImageIcon(icon3.getImage().getScaledInstance(Boton3.getWidth(), Boton3.getHeight(),Image.SCALE_SMOOTH)));
         Boton3.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2, true));
+        Boton3.addActionListener(this);
         panel.add(Boton3);
 
-        JButton Boton4 = new JButton();
+        Boton4 = new JButton();
         Boton4.setBounds(200,380,150,200);
-        ImageIcon icon4 = new ImageIcon("imagenes/4.jpg");
+        ImageIcon icon4 = new ImageIcon("imagenes/Proximos4.jpg");
         Boton4.setIcon(new ImageIcon(icon4.getImage().getScaledInstance(Boton4.getWidth(), Boton4.getHeight(),Image.SCALE_SMOOTH)));
         Boton4.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2, true));
+        Boton4.addActionListener(this);
         panel.add(Boton4);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == Boton1 || e.getSource() == Boton2 || e.getSource() == Boton3 || e.getSource() == Boton4) {
+            System.out.println("Papi rico");
+        }
     }
 }
--- src/co/edu/uptc/presentacion/RunSogaCine.java
+++ src/co/edu/uptc/presentacion/RunSogaCine.java
@@ -3,19 +3,23 @@ package co.edu.uptc.presentacion;
 import co.edu.uptc.presentacion.GUI.FrameCartelera;
 import co.edu.uptc.presentacion.GUI.FrameMenu;
 import co.edu.uptc.presentacion.GUI.FrameRegistrar;
+import co.edu.uptc.presentacion.GUI.FrameEstrenos;
 
 public class RunSogaCine {
 
     static FrameRegistrar frameRegistrar = new FrameRegistrar();
     static FrameCartelera frameCartelera = new FrameCartelera();
+    static FrameEstrenos frameEstrenos = new FrameEstrenos();
+
     static FrameMenu frameMenu = new FrameMenu();
     public static void main(String[] args) {
 //        new Menu().Inicio();
         System.out.println("Bienvenido");
         System.out.println("Ejecutando");
-        frameRegistrar.setVisible(true);
-//        frameCartelera.setVisible(true);
+        //frameRegistrar.setVisible(true);
+        frameCartelera.setVisible(true);
 //        frameMenu.setVisible(true);
+        //frameEstrenos.setVisible(true);
     }
 
 //    public void frameRegistro(){
