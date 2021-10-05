55176e67861f1704d27b10e0976e17fcf1a2b950
Juaneto <Juan.gonzalez36@uotc.edu.co>
2021-10-04 22:25:59 -0500

    TodasMuerenPorMI

--- src/co/edu/uptc/presentacion/RunSogaCine.java
+++ src/co/edu/uptc/presentacion/RunSogaCine.java
@@ -8,8 +8,9 @@ public class RunSogaCine {
     static FrameCartelera frameCartelera = new FrameCartelera();
     static FrameEstrenos frameEstrenos = new FrameEstrenos();
     static FrameIngreso frameIngreso = new FrameIngreso();
-
+    static FrameInicio frameInicio = new FrameInicio();
     static FrameMenu frameMenu = new FrameMenu();
+
     public static void main(String[] args) {
 //        new Menu().Inicio();
         System.out.println("Bienvenido");
@@ -18,7 +19,8 @@ public class RunSogaCine {
 //        frameCartelera.setVisible(true);
 //        frameMenu.setVisible(true);
 //        frameEstrenos.setVisible(true);
-        frameIngreso.setVisible(true);
+//        frameIngreso.setVisible(true);
+        frameInicio.setVisible(true);
     }
 
 }
